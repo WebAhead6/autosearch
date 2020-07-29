df8db31b3b5120b66a961978cf4cd4eca0d76d20
mervat <mervat.yosef.14@gmail.com>
2020-07-29 17:55:41 +0300

    localhost deleted

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 4000;
 
 const router = require("./router");
 
-http.createServer(router).listen(port, hostname, () => {
+http.createServer(router).listen(port, () => {
   console.log(`Server running at port http://${hostname}:${port}`);
 });
